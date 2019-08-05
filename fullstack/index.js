dfd6d0bbe323512b4e6c646ee629383aabcd25ae
Hardik Soni <sonihardik99@gmail.com>
2019-08-05 22:16:00 +0530

    changing index.js file

--- fullstack/index.js
+++ fullstack/index.js
@@ -1,3 +1,26 @@
-var name = "Hardik";
-name = "Harshita";
-alert(name);
+/* let fname = "Hardik";
+let lname = "Soni";
+let age = prompt("What's Hardik's age ?");
+
+let result = `${fname} ${lname} is ${age} years old`;
+alert(result); */
+
+/* function welcome(userName= "Guest", message="Good Day"){
+    alert(`Hello ${userName} ${message}`);
+}
+
+//welcome("Harshita", 'Good Afternoon');
+welcome(); */
+
+let createBlog = (title, body) => {
+    if(!title){
+        throw new Error("A title is required");
+    }
+    if(!body){
+        throw new Error("Body can't be empty");
+    }
+    return alert(`${title} - ${body}`);
+};
+
+createBlog("Title", "body");
+
